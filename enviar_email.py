75cc26466707daab59f0fbab6928f4c4ff0c2a1b
Jonatas Freitas <jonatasfreitas14@hotmail.com>
2022-07-01 23:10:45 -0300

    Inserido Estilização ao Programa

--- enviar_email.py
+++ enviar_email.py
@@ -1,5 +1,7 @@
+from asyncio import events
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from typing import Literal
 import PySimpleGUI as sg
 import smtplib
 
@@ -7,13 +9,11 @@ import smtplib
 class CreateWindow():
     def __init__(self):
 
-        # Trocar o Tema da Aplicação: 
-        sg.theme('SystemDefault') 
-        # ['Black', 'BlueMono', 'BluePurple', 'BrightColors', 'BrownBlue', 'Dark', 'Dark2', 'DarkAmber', 'DarkBlack', 'DarkBlack1', 'DarkBlue', 'DarkBlue1', 'DarkBlue10', #'DarkBlue11', 'DarkBlue12', 'DarkBlue13', 'DarkBlue14', 'DarkBlue15', 'DarkBlue16', 'DarkBlue17', 'DarkBlue2', 'DarkBlue3', 'DarkBlue4', 'DarkBlue5', 'DarkBlue6', #'DarkBlue7', 'DarkBlue8', 'DarkBlue9', 'DarkBrown', 'DarkBrown1', 'DarkBrown2', 'DarkBrown3', 'DarkBrown4', 'DarkBrown5', 'DarkBrown6', 'DarkBrown7', 'DarkGreen', #'DarkGreen1', 'DarkGreen2', 'DarkGreen3', 'DarkGreen4', 'DarkGreen5', 'DarkGreen6', 'DarkGreen7', 'DarkGrey', 'DarkGrey1', 'DarkGrey10', 'DarkGrey11', 'DarkGrey12', #'DarkGrey13', 'DarkGrey14', 'DarkGrey15', 'DarkGrey2', 'DarkGrey3', 'DarkGrey4', 'DarkGrey5', 'DarkGrey6', 'DarkGrey7', 'DarkGrey8', 'DarkGrey9', 'DarkPurple', #'DarkPurple1', 'DarkPurple2', 'DarkPurple3', 'DarkPurple4', 'DarkPurple5', 'DarkPurple6', 'DarkPurple7', 'DarkRed', 'DarkRed1', 'DarkRed2', 'DarkTanBlue', #'DarkTeal', 'DarkTeal1', 'DarkTeal10', 'DarkTeal11', 'DarkTeal12', 'DarkTeal2', 'DarkTeal3', 'DarkTeal4', 'DarkTeal5', 'DarkTeal6', 'DarkTeal7', 'DarkTeal8', #'DarkTeal9', 'Default', 'Default1', 'DefaultNoMoreNagging', 'GrayGrayGray', 'Green', 'GreenMono', 'GreenTan', 'HotDogStand', 'Kayak', 'LightBlue', 'LightBlue1', #'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightBlue5', 'LightBlue6', 'LightBlue7', 'LightBrown', 'LightBrown1', 'LightBrown10', 'LightBrown11', 'LightBrown12', #'LightBrown13', 'LightBrown2', 'LightBrown3', 'LightBrown4', 'LightBrown5', 'LightBrown6', 'LightBrown7', 'LightBrown8', 'LightBrown9', 'LightGray1', 'LightGreen', #'LightGreen1', 'LightGreen10', 'LightGreen2', 'LightGreen3', 'LightGreen4', 'LightGreen5', 'LightGreen6', 'LightGreen7', 'LightGreen8', 'LightGreen9', 'LightGrey', #'LightGrey1', 'LightGrey2', 'LightGrey3', 'LightGrey4', 'LightGrey5', 'LightGrey6', 'LightPurple', 'LightTeal', 'LightYellow', 'Material1', 'Material2', #'NeutralBlue', 'Purple', 'Python', 'PythonPlus', 'Reddit', 'Reds', 'SandyBeach', 'SystemDefault', 'SystemDefault1', 'SystemDefaultForReal', 'Tan', 'TanBlue', #'TealMono', 'Topanga']
-
+        # Trocando o tema da aplicação
+        sg.theme(sg.user_settings_get_entry('theme', None))
+        
         layout = [
             # Inserção de E-mail e Senha...
-
             [sg.Text(text='Email: ', size=(10)), sg.Input(default_text='',font='Arial 10', size=(50), k='email_from')],
             [sg.Text(text='Senha: ', size=(10)), sg.Input(password_char='*',default_text='',size=(50), k='pass')],
 
@@ -40,7 +40,7 @@ class CreateWindow():
             [sg.Text('                      Não escreva nada na linha abaixo...')],
             [sg.Text(text='Verificação: ', size=(10)), sg.Output(size=(48,2), background_color='Grey', text_color='white')],
             [sg.Text(''*30)],
-            [sg.Button(button_text='Enviar', k='send'), sg.Button(button_text='Cancelar', k='cancel')],
+            [sg.Button(button_text='Enviar', k='send'), sg.Button(button_text='Cancelar', k='cancel'), sg.Button(button_text='Estilos', k='theme_color')],
         ]
         # Gerando a tela.
         self.py_windows = sg.Window(title='Send Mail', layout=layout, finalize=True, resizable=True)
@@ -67,10 +67,8 @@ class CreateWindow():
                     if self.values[0] == True:
                         # Nesta variavel insira a assinatura desejada
                         msg['describe'] = self.values['describe'] + """
-                        
-                        
-                    Coloque aqui sua assinatura.
-                    Celular/Email/Dados Importantes"""
+
+                        """
                     else:
                         # Caso não queira assinatura, o corpo do e-mail terá somente a mensagem, sem assinatura.
                         msg['describe'] = self.values['describe']
@@ -114,10 +112,15 @@ class CreateWindow():
                     
                     # Caso não der certo, aparecerá está mensagem. 
                     print("ERRO! Falha no envio...")
-           #elif self.events == 'cancel':
-              #  break
-          
 
-          
+            # Modificar o Tema da aplicação
+            elif self.events in ('theme_color'):
+                self.evs, self.vals = sg.Window('Choose Theme', [[sg.Combo(sg.theme_list(), k='-THEME LIST-'), sg.OK(), sg.Cancel(button_text='Cancelar')]]).read(close=True)
+                if self.evs == 'OK':
+                    self.py_windows.Close()
+                    sg.user_settings_set_entry('theme', self.vals['-THEME LIST-'])
+                    CreateWindow().StartGUI()  
+
+
 # Criando e Iniciando a Janela                                  
 py_windows = CreateWindow().StartGUI()                       
